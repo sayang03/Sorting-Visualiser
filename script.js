7cbbea4dea06bc944d56d1a15e18f046bf659644
Sayan Ghosh <sayanedu03@gmail.com>
2024-07-14 05:51:15 +0530

    Responsiveness is improved.

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ var inp_as = document.getElementById("a_size"),
   array_size = inp_as.value;
 var inp_gen = document.getElementById("a_generate");
 var inp_aspeed = document.getElementById("a_speed");
-//var array_speed=document.getElementById('a_speed').value;
-
 var butts_algos = document.querySelectorAll(".algos button");
 
 var div_sizes = [];
